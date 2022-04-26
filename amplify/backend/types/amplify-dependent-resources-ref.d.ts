8b4a7b439f26cbaf34ac217b4dda0abfd3d50cf0
Daniil Shcherbakov <djdaniil46@gmail.com>
2022-04-26 15:13:31 +0200

    test

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3amplifyappstorage37ab71f5": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
